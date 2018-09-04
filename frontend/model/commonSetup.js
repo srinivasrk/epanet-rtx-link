e6390a26d52d449bc35d538f3a9e57bd9490b8ee
Sam Hatchett <samhatchett@gmail.com>
2018-09-04 11:38:37 -0400

    session secret is generated on-the-fly because sometimes you want to run multiple LINK instances

--- frontend/model/commonSetup.js
+++ frontend/model/commonSetup.js
@@ -4,14 +4,16 @@ var session = require('express-session')
 var cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload');
 var MemoryStore = require('session-memory-store')(session);
+const Guid = require('guid');
 
 module.exports.init = (app) => {
+  const guidSecret = Guid.create().value;
   app.use(compression({ threshold: 0 }));
   app.use(bodyParser.json({limit: '50mb'}))
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(
   	session({
-    	secret: 'fenestrate.winnow.snaketar',
+    	secret: guidSecret,
   		store: new MemoryStore(),
     	resave: false,
     	saveUninitialized: false,
