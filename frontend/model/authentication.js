9a4a8ae2a85f6fda40c11318b5fe1da1e88b0a07
Sam Hatchett <samhatchett@gmail.com>
2018-09-04 12:37:53 -0400

    view cookies

--- frontend/model/authentication.js
+++ frontend/model/authentication.js
@@ -84,6 +84,11 @@ module.exports.authenticate = function(req,res,next) {
 };
 
 module.exports.check = function(req, res, next){
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies)
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies)
+    
     if(req.isAuthenticated()){
       next();
     } else {
